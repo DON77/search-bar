f599ad7874c459dbe3c721aed2b5ed98bb808830
arm.sadoyan <arm.sadoyan@gmail.com>
2018-12-27 13:34:36 +0400

    Updates

---
+++ precache-manifest.2f23100c98c690fcb21a7ff6207a77c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "debc525e78b7873a278a",
+    "url": "/search-bar/static/css/main.64dd4987.chunk.css"
+  },
+  {
+    "revision": "debc525e78b7873a278a",
+    "url": "/search-bar/static/js/main.debc525e.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/search-bar/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "f7a59811a83969cd5dba",
+    "url": "/search-bar/static/js/runtime~main.f7a59811.js"
+  },
+  {
+    "revision": "9d76c6bd2c3b211ee25281969a3dab06",
+    "url": "/search-bar/index.html"
+  }
+];
